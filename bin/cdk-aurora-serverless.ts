590ebba1e2d4aa8b431e8ecb06512c353202a659
Scott Nixon <57370440+scottnixonjr@users.noreply.github.com>
2020-02-19 16:05:26 -0800

    Renamed stack

--- bin/cdk-aurora-serverless.ts
+++ bin/cdk-aurora-serverless.ts
@@ -6,4 +6,4 @@ import { CdkAuroraServerlessStack } from '../lib/cdk-aurora-serverless-stack';
 const envLabs = { region: 'us-west-2', account: '324320755747' }
 
 const app = new cdk.App();
-new CdkAuroraServerlessStack(app, 'AuroraPostgresServerlessStack', { env: envLabs });
+new CdkAuroraServerlessStack(app, 'AuroraPostgresServerless', { env: envLabs });
