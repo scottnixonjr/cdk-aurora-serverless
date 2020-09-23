9e801699b020d443c9b142b633b5a30fcfda4b87
Scott Nixon <57370440+scottnixonjr@users.noreply.github.com>
2020-09-23 09:29:21 -0700

    Update cdk-aurora-serverless.ts

--- bin/cdk-aurora-serverless.ts
+++ bin/cdk-aurora-serverless.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkAuroraServerlessStack } from '../lib/cdk-aurora-serverless-stack';
 
-const envLabs = { region: 'us-west-2', account: '324320755747' }
+const envLabs = { region: 'us-west-2', account: '' }
 
 const app = new cdk.App();
 new CdkAuroraServerlessStack(app, 'AuroraPostgresServerless', { env: envLabs });
